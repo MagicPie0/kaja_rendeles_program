0a7455643f8c0fafbf39fc27963548de0d5f9f5f
MagicPie0 <fulopkrisztian05@gmail.com>
2023-05-13 10:04:38 +0200

    termek_class

---
+++ WpfApp1/WpfApp1/termek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class termek
+    {
+        int ar, akcio;
+        string nev;
+        public termek(int ar, int akcio, string nev)
+        {
+            this.ar = ar;
+            this.akcio = akcio;
+            this.nev = nev;
+        }
+
+        public int Ar { get => ar; set => ar = value; }
+        public int Akcio { get => akcio; set => akcio = value; }
+        public string Nev { get => nev; set => nev = value; }
+
+    }
+}
