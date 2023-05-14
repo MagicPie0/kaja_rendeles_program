e3821002569fba0614b4d2f8e35c9bc2bfc688c5
MagicPie0 <fulopkrisztian05@gmail.com>
2023-05-14 21:25:05 +0200

    coupon1

--- WpfApp1/WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
+++ WpfApp1/WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("taco.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("arrow.png")]
 
 
